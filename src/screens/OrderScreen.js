c7aa15e5b2365bbdde2980f602d0374a2e44a7ef
Hamid Ghanbari <gh.hamid89@gmail.com>
2023-07-28 15:29:36 +0200

    List product on order screen

--- src/screens/OrderScreen.js
+++ src/screens/OrderScreen.js
@@ -1,24 +1,47 @@
-import React, { useEffect, useContext } from "react";
+import React, { useEffect, useContext, useState } from "react";
 import {
   Alert,
   Avatar,
   Box,
+  Card,
+  CardActionArea,
+  CardContent,
+  CardMedia,
   CircularProgress,
   Grid,
   List,
   ListItem,
+  Typography,
 } from "@mui/material";
+
 import Logo from "../component/Logo";
 import { styles } from "../styles";
-import { listCategories } from "../action";
+import { listCategories, listProducts } from "../action";
 import { Store } from "../Store";
 
 export default function OrderScreen() {
+  const [categoryName, setCategoryName] = useState("");
   const { state, dispatch } = useContext(Store);
   const { categories, loading, error } = state.categoryList;
+  const {
+    products,
+    loading: loadingProducts,
+    error: errorProducts,
+  } = state.productList;
+
   useEffect(() => {
-    listCategories(dispatch);
-  }, [dispatch]);
+    if (!categories) {
+      listCategories(dispatch);
+    } else {
+      listProducts(dispatch, categoryName);
+    }
+  }, [dispatch, categories, categoryName]);
+
+  const categoryClickHandler = (name) => {
+    setCategoryName(name);
+    listProducts(dispatch, categoryName);
+  };
+  console.log(categories, products);
   return (
     <Box className={styles.root}>
       <Box className={styles.main}>
@@ -31,11 +54,14 @@ export default function OrderScreen() {
                 <Alert severity="error">{error}</Alert>
               ) : (
                 <>
-                  <ListItem button>
-                    <Logo l></Logo>
+                  <ListItem onClick={() => categoryClickHandler("")} button>
+                    <Logo></Logo>
                   </ListItem>
                   {categories.map((category) => (
-                    <ListItem button key={category.name}>
+                    <ListItem
+                      button
+                      key={category.name}
+                      onClick={() => categoryClickHandler(category.name)}>
                       <Avatar alt={category.name} src={category.image} />
                     </ListItem>
                   ))}
@@ -44,7 +70,58 @@ export default function OrderScreen() {
             </List>
           </Grid>
           <Grid item md={10}>
-            Food list
+            <Typography
+              gutterBottom
+              className={styles.title}
+              variant="h2"
+              component="h2">
+              {categoryName || "Main Menu"}
+            </Typography>
+            <Grid container spacing={1}>
+              {loadingProducts ? (
+                <CircularProgress />
+              ) : errorProducts ? (
+                <Alert severity="error">{errorProducts}</Alert>
+              ) : (
+                products.products.map((product) => (
+                  <Grid item md={6}>
+                    <Card className={styles.card}>
+                      <CardActionArea>
+                        <CardMedia
+                          component="img"
+                          alt={product.name}
+                          image={product.image}
+                          className={styles.media}
+                        />
+                      </CardActionArea>
+                      <CardContent>
+                        <Typography
+                          gutterBottom
+                          variant="body2"
+                          color="textPrimary"
+                          component="p">
+                          {product.name}
+                        </Typography>
+                        <Box className={styles.cardFooter}>
+                          <Typography
+                            variant="body2"
+                            color="textSecondary"
+                            component="p">
+                            {product.calorie} Cal
+                          </Typography>
+                          <Typography
+                            variant="body2"
+                            color="textPrimary"
+                            component="p">
+                            ${product.price}
+                          </Typography>
+                        </Box>
+                      </CardContent>
+                    </Card>
+                  </Grid>
+                ))
+              )}
+            </Grid>
           </Grid>
         </Grid>
       </Box>
